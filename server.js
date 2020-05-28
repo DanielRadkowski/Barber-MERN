959ebdf2b2b98c27eaae403b8853e182adf657ca
EnigmatycznyAndrzej <daniel.radkowski@gmail.com>
2020-05-28 23:02:26 +0200

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
-
+//const cors = require('cors');
+const path = require('path');
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -18,7 +18,13 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
-app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {           
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const barberRouter = require('./routes/barber');
 
