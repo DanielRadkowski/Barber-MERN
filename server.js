ba7460d417b5615566891299ac2cd7ae93496491
EnigmatycznyAndrzej <daniel.radkowski@gmail.com>
2020-05-29 22:34:56 +0200

    cors again

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+//const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors({origin: true, credentials: true}));
+//app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.use(express.json());
 
