79d50b5edf1e443b06b9a45211edb700e18cc310
EnigmatycznyAndrzej <daniel.radkowski@gmail.com>
2020-05-29 19:05:26 +0200

    cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+//const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
